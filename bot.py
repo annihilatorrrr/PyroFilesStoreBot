1e36e3730fd8ce4bc971862f27287e1955644605
Abir Hasan <hackitabirhasan@gmail.com>
2021-04-05 15:23:06 +0600

    As Wanted

--- bot.py
+++ bot.py
@@ -57,43 +57,6 @@ async def start(bot, cmd):
 		)
 	usr_cmd = cmd.text.split("_")[-1]
 	if usr_cmd == "/start":
-		if Config.UPDATES_CHANNEL:
-			invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
-			try:
-				user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.from_user.id)
-				if user.status == "kicked":
-					await bot.send_message(
-						chat_id=cmd.from_user.id,
-						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-						parse_mode="markdown",
-						disable_web_page_preview=True
-					)
-					return
-			except UserNotParticipant:
-				await bot.send_message(
-					chat_id=cmd.from_user.id,
-					text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
-					reply_markup=InlineKeyboardMarkup(
-						[
-							[
-								InlineKeyboardButton("🤖 Join MEMES Channel", url=invite_link.invite_link)
-							],
-							[
-								InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
-							]
-						]
-					),
-					parse_mode="markdown"
-				)
-				return
-			except Exception:
-				await bot.send_message(
-					chat_id=cmd.from_user.id,
-					text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-					parse_mode="markdown",
-					disable_web_page_preview=True
-				)
-				return
 		await cmd.reply_text(
 			HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id),
 			parse_mode="Markdown",
